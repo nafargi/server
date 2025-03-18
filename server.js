89b31c7929f31d934973834aeb454617be75dbaa
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-03-18 18:23:36 +0300

    adding specific artist based on id

--- server.js
+++ server.js
@@ -247,6 +247,28 @@ app.get('/api/fetchTracks/playlist/:id', async (req, res) => {
 
 // Fetch artist's albums
 
+app.get('/api/artist/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await fetch(`https://api.deezer.com/artist/${id}`);
+    
+    // Check if the response is not HTML (we expect JSON)
+    const contentType = response.headers.get('Content-Type');
+    if (!contentType || !contentType.includes('application/json')) {
+      throw new Error('Expected JSON, but got something else');
+    }
+
+    if (!response.ok) {
+      throw new Error(`Error fetching ARTIST for ${id}: ${response.statusText}`);
+    }
+    
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(`Error fetching ARTIST for ${id}:`, error.message);
+    res.status(500).json({ error: `Error fetching ARTIST for ${id}` });
+  }
+});
 
 app.get('/api/artist/:id/albums', async (req, res) => {
   try {
