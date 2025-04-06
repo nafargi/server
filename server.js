aa99427e88592274d63ac42cec63d6919af0ea6d
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-04-06 22:06:22 +0300

    comment out require

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const rateLimit = (windowMs, max) => {
   };
 };
 
-const require = createRequire(import.meta.url);
+// const require = createRequire(import.meta.url);
 // const { AudioContext } = NodeWebAudioApi;
 
 const app = express();
